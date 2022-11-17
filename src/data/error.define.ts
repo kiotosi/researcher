4e8698213f63cb82edae6ed0edefa07450ef5fec
Даниил Шило <crackidocky@gmail.com>
2022-11-17 23:44:59 +0300

    feat(tag): add service bus for tags.json

--- src/data/error.define.ts
+++ src/data/error.define.ts
@@ -2,13 +2,15 @@ export default {
   load: {
     file: {
       workspace: 'Can\'t load workspace.json in configuration folder!',
-      config: 'Can\'t load config.json in workspace folder!'
+      config: 'Can\'t load config.json in workspace folder!',
+      tag: "Can't load tags.json in workspace folder!",
     }
   },
   save: {
     file: {
       workspace: 'Can\'t save new workspace to workspace.json!',
-      config: 'Can\'t save new configuration to config.json!'
+      config: 'Can\'t save new configuration to config.json!',
+      tag: "Can't save tags.json in workspace folder!",
     }
   }
 }
--- src/data/path.define.ts
+++ src/data/path.define.ts
@@ -1,6 +1,7 @@
 export const CONFIG_ROOT = 'Researcher';
 export const WORKSPACE_JSON = 'workspace.json';
 export const CONFIG_JSON = 'config.json';
+export const TAGLIST_JSON = 'tags.json';
 export const LINK_JSON = 'links.json';
 export const VIDEO_JSON = 'videos.json';
 export const NOTE_JSON = 'notes.json';
