ec63aa96d0d0e3fcbbec513e62093d7ba11464d8
Даниил Шило <crackidocky@gmail.com>
2022-11-17 04:09:50 +0300

    feat(design): add two design component

--- src/components/design/InputMain.vue
+++ src/components/design/InputMain.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts">
+defineProps({
+  placeholder: {
+    type: String,
+    default: ''
+  }
+});
 </script>
 
-<template />
+<template>
+  <input :placeholder="placeholder" class="input" type="text">
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.input {
+  background-color: var(--color-normal-20);
+  border: 1px solid var(--color-normal-30);
+  color: var(--color-normal-800);
+  outline: none;
+  padding: 10px 12px;
+  border-radius: 4px;
+  font-weight: 500;
+
+  &::placeholder {
+    color: var(--color-normal-90);
+  }
+}
+</style>
